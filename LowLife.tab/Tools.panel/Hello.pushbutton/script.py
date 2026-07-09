d2681d7b0a6c4c3e03bb31a67ba6016bff27d5f9
avpersiyanov-afk <avpersiyanov@gmail.com>
2026-07-10 00:49:37 +0300

    Delete LowLife.tab/Tools.panel/Hello.pushbutton directory

--- LowLife.tab/Tools.panel/Hello.pushbutton/script.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-__title__ = "Привет"
-__doc__ = "Тестовая кнопка компании"
-__author__ = "BIM-отдел"
-
-from pyrevit import forms
-
-forms.alert("Корпоративный инструмент работает! 🎉",
-            title="CompanyTools")
