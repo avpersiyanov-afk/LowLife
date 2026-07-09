595a72a581ac733375f59eee7d4d644aa79cd396
avpersiyanov-afk <avpersiyanov@gmail.com>
2026-07-10 00:40:32 +0300

    Update script.py

--- LowLife.tab/Tools.panel/Hello.pushbutton/script.py
+++ LowLife.tab/Tools.panel/Hello.pushbutton/script.py
@@ -1,2 +1,9 @@
+# -*- coding: utf-8 -*-
+__title__ = "Привет"
+__doc__ = "Тестовая кнопка компании"
+__author__ = "BIM-отдел"
+
 from pyrevit import forms
-forms.alert("Работает!")
+
+forms.alert("Корпоративный инструмент работает! 🎉",
+            title="CompanyTools")
