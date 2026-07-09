1aa1bbb97a69c33eb220ff18fe74c9fca3023a04
Andrey Persiyanov <avpersiyanov@gmail.com>
2026-07-10 00:59:46 +0300

    Update script.py

--- LowLife.tab/Tools.panel/Hello.pushbutton/script.py
+++ LowLife.tab/Tools.panel/Hello.pushbutton/script.py
@@ -1,1 +1,9 @@
+# -*- coding: utf-8 -*-
+__title__ = "Привет"
+__doc__ = "Тестовая кнопка компании PopovBimTools"
+__author__ = "Попов"
 
+from pyrevit import forms
+
+forms.alert("Корпоративный инструмент работает! 🎉", 
+            title="PopovBimTools")
