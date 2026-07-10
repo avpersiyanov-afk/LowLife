468d4fa60a1f6a308c59edc872ba3408f689f65d
Andrey Persiyanov <avpersiyanov@gmail.com>
2026-07-10 14:42:31 +0300

    Update script.py

--- LowLife.tab/Music.panel/Player.pushbutton/script.py
+++ LowLife.tab/Music.panel/Player.pushbutton/script.py
@@ -1,54 +1,97 @@
 # -*- coding: utf-8 -*-
 
 from pyrevit import forms
+from pyrevit import revit
+
 import ctypes
+
+from System.Windows.Interop import WindowInteropHelper
+from Autodesk.Revit.UI import UIApplication
 
 
 VK_MEDIA_PLAY_PAUSE = 0xB3
 VK_MEDIA_NEXT = 0xB0
 VK_MEDIA_PREV = 0xB1
+
 VK_VOLUME_DOWN = 0xAE
 VK_VOLUME_UP = 0xAF
 
 
+
 def press_key(key):
 
-    ctypes.windll.user32.keybd_event(key, 0, 0, 0)
-    ctypes.windll.user32.keybd_event(key, 0, 2, 0)
+    ctypes.windll.user32.keybd_event(
+        key,
+        0,
+        0,
+        0
+    )
+
+    ctypes.windll.user32.keybd_event(
+        key,
+        0,
+        2,
+        0
+    )
 
 
 
 class Player(forms.WPFWindow):
 
     def __init__(self):
+
         forms.WPFWindow.__init__(
             self,
             "Player.xaml"
         )
 
 
+        # Получаем главное окно Revit
+        uiapp = UIApplication(
+            revit.HOST_APP.app
+        )
+
+        revit_handle = uiapp.MainWindowHandle
+
+
+        # Привязываем окно к Revit
+        helper = WindowInteropHelper(self)
+
+        helper.Owner = revit_handle
+
+
+
     def PlayButton_Click(self, sender, args):
+
         press_key(VK_MEDIA_PLAY_PAUSE)
 
 
+
     def NextButton_Click(self, sender, args):
+
         press_key(VK_MEDIA_NEXT)
 
 
+
     def PrevButton_Click(self, sender, args):
+
         press_key(VK_MEDIA_PREV)
 
 
+
     def VolUpButton_Click(self, sender, args):
+
         press_key(VK_VOLUME_UP)
 
 
+
     def VolDownButton_Click(self, sender, args):
+
         press_key(VK_VOLUME_DOWN)
 
 
 
-# сохраняем окно глобально
-window = Player()
+# сохраняем ссылку
+player_window = Player()
 
-window.Show()
+player_window.Show()
